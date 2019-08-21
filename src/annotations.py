0fc2b1166380b7e0d52c3707facf17e009350304
chivers <dan.chivers@berkeleyappliedanalytics.com>
2019-08-21 16:27:25 -0700

    Added beat annotation location function

--- src/annotations.py
+++ src/annotations.py
@@ -7,3 +7,23 @@ def getAnnotations(file):
     ext = ext.replace('.','')
     ann = rdann(base, ext)
     return ann.sample, ann.symbol, ann.subtype
+
+
+def extractBeatAnnotationLocations(ann):
+
+    symlist = 'NLRBAaJSVrFejnE/fQ'
+
+    loc, sym, sub = ann
+
+    pk = []
+    s = []
+
+    for i in range(len(sym)):
+        if sym[i] in symlist:
+            pk.append(loc[i])
+            s.append(sym[i])
+
+    return pk, s
+
+
+
