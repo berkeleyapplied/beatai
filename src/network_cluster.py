cebc9849f91a34590428b162cdf4222c7bea1ee3
chivers <dan.chivers@berkeleyappliedanalytics.com>
2019-08-21 21:22:42 -0700

    added temporal network connection, communityZScore for a given signal, full plotting of classes and annotations, added label creation at end of runNetworkCluster.

--- src/network_cluster.py
+++ src/network_cluster.py
@@ -7,8 +7,12 @@ from cluster import *
 import time
 from annotations import getAnnotations, extractBeatAnnotationLocations
 
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
 
-def create_signal_network(dataset, depth=5):
+def create_signal_network(dataset, depth=5, rng=200):
     # plt.ion()
     ind = range(dataset.shape[0])
 
@@ -24,12 +28,24 @@ def create_signal_network(dataset, depth=5):
     conn = []
 
     for i in ind:
-        dd = dataset - dataset[i][:]
+        r1 = i-int(rng/2)
+        r2 = i+int(rng/2)
+        if r1<0:
+            r1 = 0
+            r2 = rng
+        if r2 > len(ind):
+            r1 = len(ind)-rng
+            r2 = len(ind)
+
+        sind = np.arange(r1,r2)
+
+        dd = dataset[sind][:] - dataset[i][:]
+        #dd = dataset - dataset[i][:]
         dist = np.sqrt(np.sum(np.power(dd,2.0),1))
-        dist[i] = np.max(dist)
+        dist[sind==i] = np.max(dist)
         sarr = []
-        for j in range(len(dist)):
-            sarr.append((j,dist[j]))
+        for j in range(len(sind)):
+            sarr.append((sind[j],dist[j]))
         sarr.sort(key=lambda tup: tup[1])
 
         for j in range(depth):
@@ -54,7 +70,7 @@ def cluster_signals(dset, depth=5):
     for i in range(len(G.nodes)):
         cls.append(partition[i])
     C = {}
-    for c in range(max(cls)):
+    for c in range(max(cls)+1):
         C[c] = []
         for i in range(len(G.nodes)):
             if partition[i] == c:
@@ -96,6 +112,22 @@ def community_envelope(dataset, cls, ncls, plotflag=False):
 
     return Sm, Sstd
 
+def communityZScore(dataset, cls, ncls, n, plotflag=False):
+
+    nx = int(dataset.shape[1]/2)
+    Sm, Sstd = community_envelope(dataset, cls, ncls);
+    z = np.abs(dataset[n][:nx]-Sm)/Sstd
+    if plotflag:
+        fig,(ax1,ax2) = plt.subplots(2,1,sharex=True)
+        plot_community_envelope(dataset, cls, ncls, nstd=1.0)
+        ax2.plot(dataset[n][:nx],'r')
+        ax1.plot(z)
+        plt.show()
+    return np.mean(z), z
+
+
+
+
 def plot_community_envelope(dataset, cls, ncls, nstd=0.0, clr='k'):
     ind = cls[ncls]
 
@@ -120,10 +152,27 @@ def plot_community_envelope(dataset, cls, ncls, nstd=0.0, clr='k'):
         plt.plot(Sm - nstd*Sstd, '--',color=clr)
     plt.legend()
 
-def community_joint_distribution(dataset, cls, acls, bcls, plotflag=False):
+def plot_community_symbols(root, datan, cls, pk1):
+    datfile = '{}/{}.dat'.format(root, datan)
+    atrfile = '{}/{}.atr'.format(root, datan)
+    ann = getAnnotations(atrfile)
+    pk, sym = extractBeatAnnotationLocations(ann)
+    sym = sym[1:-1]
 
-    sma, stda = community_envelope(dataset, cls, acls)
-    smb, stdb = community_envelope(dataset, cls, bcls)
+    D1, D2 = retrieveBatch(datfile, None, None, True)
+    s1, p, c = D1
+
+    plt.plot(s1,'b')
+    ncls = len(cls.keys())
+    cmap = get_cmap(ncls)
+    nclr = 0
+    for k in cls:
+        ind = cls[k]
+        plt.plot(pk1[ind],s1[pk1[ind]],'.',color=cmap(nclr))
+        nclr += 1
+        for i in ind:
+            plt.text(pk[i],s1[pk[i]]+20.0,'{}-{}'.format(sym[i],k))
+    plt.show()
 
 
 def runNetworkCluster(root, datan, useAnn=True, filter=30.0, depth=5):
@@ -140,8 +189,11 @@ def runNetworkCluster(root, datan, useAnn=True, filter=30.0, depth=5):
 
     dset = convertSignalMatrixToDataset(sr,si)
     G, cls, part = cluster_signals(dset, depth=depth)
+    # TODO: re-cluster...combine...etc...
 
-    return dset, G, cls
+    G = label_community_nodes(G, part)
+
+    return dset, G, cls, part, pk1
 
 
 
